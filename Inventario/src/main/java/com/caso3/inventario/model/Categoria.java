7c40d1f26e087e74bbafe66a2ffc40c5f33e740c
MemoLTS <guil.toledo@duocuc.cl>
2026-06-29 17:53:03 -0400

    "si"

--- Inventario/src/main/java/com/caso3/inventario/model/Categoria.java
+++ Inventario/src/main/java/com/caso3/inventario/model/Categoria.java
@@ -2,12 +2,22 @@ package com.caso3.inventario.model;
 
 public enum Categoria {
     ALIMENTOS,
+    FRUTA,
+    VERDURA,
+    HIERBAS,
+    ESPECIAS,
     DULCES,
     BEBIDAS,
     LIMPIEZA,
     CUIDADO_PERSONAL,
+    COSMETICA,
+    ALCOHOL,
+    TABACO,
+    FARMACEUTICO,
     ROPA,
     HOGAR,
+    JOYERIA,
+    MUEBLE,
     ELECTRODOMESTICOS,
     OTROS
 }
